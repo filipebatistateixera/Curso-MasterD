4cb65ba82a319f10a6d9f6fc17dade6df6b07424
Filipe Batista Teixeira <filipebt3@gmail.com>
2024-06-11 21:31:04 +0100

    atualização

--- JavaScript/Exercícios/Casoprático-JavaScriptAJAX/scripts/scripts.js
+++ JavaScript/Exercícios/Casoprático-JavaScriptAJAX/scripts/scripts.js
@@ -1,32 +1,23 @@
 
+//NOTÍCIAS RSS
 
-
-
-
-//MAPA
-function carregarmapa() {
-    var ponto = new google.maps.LatLng(
-        38.804098, -9.200102);
-    var opcoes = {
-        zoom: 12,
-        center: ponto,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    mapa = new google.maps.Map(document.getElementById("mapa"),
-        opcoes);
-    var opcoesFotos = {
-        position: ponto,
-        pov: {
-            heading: 90,
-            pitch: 0,
-            zoom: 2 
+function carregar() {
+    var url = 'https://news.google.com/rss?hl=pt-PT&gl=PT&ceid=PT:pt-150';
+    $.ajax({
+        url:"https://api.rss2json.com/v1/api.json?rss_url=" + url,
+        type: 'GET',
+        success: function (data) {
+            objeto_json = eval(data);
+            // ler o conteúdo
+            var frase = "";
+            for (i = 0; i < objeto_json.items.length; i++) {
+                frase = frase + "Título: <b>" + objeto_json.items[i].title + "</b><br/>";
+                frase = frase + objeto_json.items[i].description + "<br/>";
+            }
+            $("#caixa").html(frase);
+        },
+        error: function (xhr, status) {
+            alert('Ocorreu um erro.');
         }
-    };
-    var fotos = new
-        google.maps.StreetViewPanorama(document.getElementById("fotos"),
-            opcoesFotos);
-    mapa.setStreetView(fotos);
+    });
 }
-
-
-
